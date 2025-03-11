0122e17a91993ef6cc66c4c790c7f121d0bfc27b
César Bernal <cesarbernalpabon2019@gmail.com>
2025-03-11 18:28:41 -0500

    se muestra alerta si el campo está vacío

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 function agregarAmigo() {
-    // Capturar el valor del input y eliminar espacios en blanco
+// Capturar el valor del input y eliminar espacios en blanco
     let input = document.getElementById('inputName');
     let nombre = input.value.trim();
-  
+// Validación: se muestra alerta si el campo está vacío
+    if (nombre === '') {
+        alert("Por favor, inserte un nombre.");
+        return;
+      }
+}    
 
 
